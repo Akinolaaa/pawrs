c93ad782591adea9d248af588abc694c6ca4f761
akinola-aktivate <60165552+Akinolaaa@users.noreply.github.com>
2023-12-26 18:19:22 +0100

    /product endpoint

--- src/app/components/AllPets.tsx
+++ src/app/components/AllPets.tsx
@@ -10,7 +10,7 @@ const playfairDisplay = Playfair_Display({
 
 export default function AllPets() {
   return (
-    <div className='p-7 mb-5 border'>
+    <div className='p-7 mb-5'>
       <h1 className={`my-5 text-3xl font-thin`}> All <span className={`${playfairDisplay.className}`}> Pets </span></h1>
       <div className='flex gap-2'>
         <PetCard />
